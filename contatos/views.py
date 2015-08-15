a00b609d67e6d2f0cea9993007d66e2d9fcd8ecd
Francisco André <fandrefh@gmail.com>
2015-08-15 18:16:40 -0300

    Forms, Templates, CRUD

--- contatos/views.py
+++ contatos/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+
+from .forms import ContatoForm
+from .models import Contato
 
 # Create your views here.
+
+def add_contato(request):
+	if request.method == 'POST':
+		form_contato = ContatoForm(request.POST)
+		if form_contato.is_valid():
+			form_contato.save()
+			return redirect(reverse('sucesso'))
+		else:
+			print(form_contato.errors)
+	else:
+		form_contato = ContatoForm()
+	return render(request, 'add_contato.html', {'form_contato': form_contato})
+
+def cadastro_sucesso(request):
+	return render(request, 'sucesso.html')
+
+def lista_contatos(request):
+	contatos = Contato.objects.all()
+	return render(request, 'lista_contatos.html', {'contatos': contatos})
+
+def apagar_contato(request, id):
+	Contato.objects.get(id=id).delete()
+	return redirect(reverse('listacontatos'))
--- urls.py
+++ urls.py
@@ -17,8 +17,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from agenda.core.views import home
+from agenda.contatos.views import add_contato
+from agenda.contatos.views import cadastro_sucesso
+from agenda.contatos.views import lista_contatos
+from agenda.contatos.views import apagar_contato
 
 urlpatterns = [
 	url(r'^$', home, name='home'),
+	url(r'^adicionar-contato/$', add_contato, name='addcontato'),
+	url(r'^sucesso/$', cadastro_sucesso, name='sucesso'),
+	url(r'^lista-contatos/$', lista_contatos, name='listacontatos'),
+	url(r'^deletar-contato/(?P<id>\d+)/$', apagar_contato, name='apagarcontato'),
     url(r'^admin/', include(admin.site.urls)),
 ]
